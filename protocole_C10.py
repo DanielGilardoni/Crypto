e5a556eb57c80dce89e9d7497a48f6b203633fdd
Mojigot7 <ayyacine.pro@gmail.com>
2023-04-17 14:48:53 +0200

    c10 fini

--- protocole_C10.py
+++ protocole_C10.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 def test_Miller_Rabin(n, k):
     if n == 2 or n == 3:
@@ -41,16 +42,80 @@ def gen_premier_sophie_germain():
     return q, n
 
 def trouver_generateur(p):
-    g = random.randint(3, p - 2)
-    while g**2 % p != 1 or g**((p - 1) // 2) % p != 1:
-        g = random.randint(3, p - 2)
-    return g
+    q = p - 1
+    factors = []
+    for i in range(2, int(math.sqrt(q))+1):
+        if q % i == 0:
+            factors.append(i)
+            while q % i == 0:
+                q //= i
+        if q > 1:
+            factors.append(q)
+
+    for g in range(2, p):
+        if all(pow(g, (p-1)//f, p) != 1 for f in factors):
+            return g
+    return None
+
+def prime_factors(n):
+    factors = []
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+    for i in range(3, int(n**0.5)+1, 2):
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+    if n > 2:
+        factors.append(n)
+    return factors
 
 # B utilise pour générer le triplet (p, g, h)
 def genB():
     p, q = gen_premier_sophie_germain()
     g = trouver_generateur(p)
     h = random.randint(2, p - 2)
-    if h**q % p != 1:
+    while h**q % p != 1:
         h = random.randint(2, p - 2)
     return p, g, h
+
+# A vérifie le triplet (p, g, h)
+def verifA(triplet):
+    p, g, h = triplet
+    if not test_Miller_Rabin(p, 10) or not test_Miller_Rabin(p - 1 // 2, 10):
+        return False
+    if pow(g, p-1, p) != 1:
+        return False
+    for f in prime_factors(p-1):
+        if pow(g, (p-1)//f, p) == 1:
+            return False
+    
+    return True
+
+def commitA(triplet, xy):
+    p, g, h = triplet
+    x, y = xy
+    c = (pow(g, 2*y, p) * pow(h, x, p)) % p
+    return c
+
+def revealA(xy):
+    return xy
+
+def verifB(triplet, c, xy):
+    p, g, h = triplet
+    x, y = xy
+    if c == pow(g, 2*y, p) * pow(h, x, p) % p:
+        return True
+    return False
+
+gen_b = genB()
+print(gen_b)
+print(verifA(gen_b))
+
+xy = (random.randint(2, gen_b[0]-1), random.randint(2, gen_b[0]-1))
+print(xy)
+
+c = commitA(gen_b, xy)
+print(c)
+
+print(verifB(gen_b, c, xy))
